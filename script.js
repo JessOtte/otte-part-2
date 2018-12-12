dd057600898caf57dd5b1f44afe61e034170a99b
semiottematic <40188802+semiottematic@users.noreply.github.com>
2018-12-12 16:48:23 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+var tempFahr = "What is the temperature?";
+var greeting1 = prompt(tempFahr, "temperature in degrees");
+
+var eventType = "What type of event are you attending?";
+var greeting2 = prompt(eventType, "casual, semi-formal, or formal");
+
+ 
+
+
+
+
+if (greeting2==="casual") {
+    var result2 = "something comfy";
+}
+if (greeting2==="semi-formal") {
+    var result2 = "a polo";
+}
+if (greeting2==="formal") {
+    var result2 = ("a suit");
+}
+
+
+
+
+if (greeting1<54) {
+    var result1 = "a coat";
+}
+else if ((greeting1>54) && (tempFahr<70)) {
+    var result1 = "a jacket";
+}
+else if (greeting1>70) {
+    var result1 = "no jacket";
+}
+
+
+alert("Since it is " + greeting1  + " degrees and you are going to a " + greeting2 + " event, you should wear " + result2 + " and " + result1 + ".");
